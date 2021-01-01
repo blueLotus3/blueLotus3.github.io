bc9d2b810030680c9f6456cd664b9ac05660507a
Troy Lemons Jr <troylemonsjr@gmail.com>
2021-01-01 11:01:04 -0800

    Portfolio Project 1

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ $.ajax("https://spreadsheets.google.com/feeds/list/1pD907D-6Bm1QTSsVk2jL2TWuzKfu
        const $div = $(`<sl-card class="projects">
        <img class="img" src="https://res.cloudinary.com/ds7w3ysag/image/upload/v1609451823/Troy_Lemons_r_-_Software_Engineer_m52qo1.jpg" width="200" height:"200"${projects[i].img}
        alt="Portfolio Project" <strong><h3 "projects">${projects[i].name}</h3></strong>
-       <p class=description  ${projects[i].description}</p>
+       <p class=description   ${projects[i].description}</p>
        <div-slot="aside">
        <sl-button class='git' href=${projects[i].github}>The Code</sl-button>
        <sl-button class='live' href=${projects[i].live}>The Project</sl-button>
